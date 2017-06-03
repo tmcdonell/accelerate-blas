76ba91573501484144d7e7d026cec4be964b7838
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2017-06-03 14:38:05 +1000

    Add foreign CPU implementation of gemm

---
+++ Data/Array/Accelerate/Numeric/LinearAlgebra/LLVM/Native/Base.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+-- |
+-- Module      : Data.Array.Accelerate.Numeric.LinearAlgebra.LLVM.Native.Base
+-- Copyright   : [2017] Trevor L. McDonell
+-- License     : BSD3
+--
+-- Maintainer  : Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
+
+module Data.Array.Accelerate.Numeric.LinearAlgebra.LLVM.Native.Base
+  where
+
+import Data.Array.Accelerate
+import Data.Array.Accelerate.Array.Sugar                            ( Array(..) )
+import Data.Array.Accelerate.Array.Data
+import Data.Array.Accelerate.Array.Unique
+import Data.Array.Accelerate.Lifetime
+import Data.Array.Accelerate.Numeric.LinearAlgebra.Type             as T
+
+import Control.Monad.ST                                             ( RealWorld )
+
+import qualified Data.Vector.Storable.Mutable                       as S
+import qualified Numerical.HBLAS.MatrixTypes                        as H
+
+
+encodeTranspose :: Transpose -> H.Transpose
+encodeTranspose N = H.NoTranspose
+encodeTranspose T = H.Transpose
+encodeTranspose H = H.ConjTranspose
+
+
+withMatrix
+    :: forall e a. Numeric e
+    => Matrix e
+    -> (H.MDenseMatrix RealWorld H.Row e -> IO a)
+    -> IO a
+withMatrix mat@(Array _ adata) go =
+  let Z :. rows :. cols = arrayShape mat
+      n                 = rows * cols
+  in
+  case (numericR :: NumericR e, adata) of
+    (NumericRfloat32, AD_Float  (UniqueArray _ ua)) -> withLifetime ua (\fp -> go (H.MutableDenseMatrix H.SRow cols rows cols (S.MVector n fp)))
+    (NumericRfloat64, AD_Double (UniqueArray _ ua)) -> withLifetime ua (\fp -> go (H.MutableDenseMatrix H.SRow cols rows cols (S.MVector n fp)))
+
---
+++ Data/Array/Accelerate/Numeric/LinearAlgebra/LLVM/Native/Level3.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+-- |
+-- Module      : Data.Array.Accelerate.Numeric.LinearAlgebra.LLVM.Native.Level3
+-- Copyright   : [2017] Trevor L. McDonell
+-- License     : BSD3
+--
+-- Maintainer  : Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
+
+module Data.Array.Accelerate.Numeric.LinearAlgebra.LLVM.Native.Level3 (
+
+  gemm,
+
+) where
+
+import Data.Array.Accelerate                                        as A
+import Data.Array.Accelerate.LLVM.Native.Foreign
+import Data.Array.Accelerate.Numeric.LinearAlgebra.Type
+import Data.Array.Accelerate.Numeric.LinearAlgebra.LLVM.Native.Base
+
+import qualified Numerical.HBLAS.BLAS.Level3                        as H
+
+
+gemm :: forall e. Numeric e => Transpose -> Transpose -> ForeignAcc ((Scalar e, Matrix e, Matrix e) -> Matrix e)
+gemm opA opB = ForeignAcc "cblas.gemm" gemm'
+  where
+    gemm' (alpha, matA, matB) = do
+      let
+          Z :. rowsA :. colsA = arrayShape matA
+          Z :. rowsB :. colsB = arrayShape matB
+
+          rowsC   = case opA of
+                      N -> rowsA
+                      _ -> colsA
+          colsC   = case opB of
+                      N -> colsB
+                      _ -> rowsB
+
+          opA'    = encodeTranspose opA
+          opB'    = encodeTranspose opB
+          alpha'  = indexArray alpha Z
+      --
+      matC  <- allocateRemote (Z :. rowsC :. colsC) :: LLVM Native (Matrix e)
+      liftIO $ do
+       withMatrix matA   $ \matA' -> do
+        withMatrix matB  $ \matB' -> do
+         withMatrix matC $ \matC' -> do
+          case numericR :: NumericR e of
+            NumericRfloat32   -> H.sgemm opA' opB' alpha' 0 matA' matB' matC' >> return matC
+            NumericRfloat64   -> H.dgemm opA' opB' alpha' 0 matA' matB' matC' >> return matC
+            NumericRcomplex32 -> H.cgemm opA' opB' alpha' 0 matA' matB' matC' >> return matC
+            NumericRcomplex64 -> H.zgemm opA' opB' alpha' 0 matA' matB' matC' >> return matC
+
