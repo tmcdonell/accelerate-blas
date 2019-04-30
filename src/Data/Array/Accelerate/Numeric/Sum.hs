71fb0325c411dc7e7886c46478fe75afd46e34b5
Trevor L. McDonell <trevor.mcdonell@gmail.com>
2019-04-30 14:29:53 +0200

    build fix for ghc-8.6

--- src/Data/Array/Accelerate/Numeric/Sum.hs
+++ src/Data/Array/Accelerate/Numeric/Sum.hs
@@ -7,6 +7,7 @@
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE UndecidableInstances  #-}
 {-# LANGUAGE ViewPatterns          #-}
 -- |
 -- Module      : Data.Array.Accelerate.Numeric.Sum
